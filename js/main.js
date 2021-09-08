6612fd77596fe5deac3a8fcc6151f0717759bc4f
StefDeVita <devitastefano@hotmail.com>
2021-09-08 15:05:44 -0300

    Añado modal de confirmacion, footer y añado mas productos al sitio

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
+
 // Interes arbitrario mensual
 const INTERESMENSUAL = 3;
 class Carrito{
     constructor(){
         this.productos = [];
         this.cantidad = 0;
+        this.total = 0;
     }
     estaVacio(){
         return this.cantidad === 0;
@@ -11,6 +13,7 @@ class Carrito{
     aniadirProducto(producto){
         this.productos.push(producto);
         this.cantidad++;
+        this.total += producto.precio;
     }
     eliminarProducto(producto){
         for (let i = 0; i < this.productos.length; i++) {
@@ -23,7 +26,7 @@ class Carrito{
             }
             
         }
-
+        this.total = this.total - producto.precio;
     }
     ordenarProductos(){
         let productosOrdenados = this.productos.sort((producto1,producto2) =>{
@@ -71,10 +74,16 @@ class Producto{
     }
 
 }
+//Creacion de los productos y del carrito
 const jujutsu = new Producto("Jujutsu Kaisen Volumen 1",500,1,"/img/jujutsu.webp");
 const haikyu = new Producto("Haikyu!! Volumen 1",500,1,"/img/haikyu.jpg");
+const jujutsu2 = new Producto("Jujutsu Kaisen Volumen 2",500,1,"/img/jujutsu2.png");
+const haikyu2 = new Producto("Haikyu!! Volumen 2",500,1,"/img/haikyu2.jpg");
+const jujutsu3 = new Producto("Jujutsu Kaisen Volumen 3",500,1,"/img/jujutsu3.jpg");
 const sincity = new Producto("Sin City Volumen 1",1000,1,"/img/sincity.jpg");
-const productos = [jujutsu,haikyu,sincity];
+const sincity2 = new Producto("Sin City Volumen 2",1000,1,"/img/sincity2.jpg");
+const metalgear =new Producto("Metal Gear Solid: Sons of Liberty 1",795,1,"img/metalgear.jpg");
+const productos = [jujutsu,haikyu,sincity,metalgear,sincity2,jujutsu2,haikyu2,jujutsu3];
 const carrito = new Carrito();
 //Añade a una tabla el producto que se añadio al carrito
 function agregarCarrito(nombre){
@@ -92,22 +101,27 @@ function agregarCarrito(nombre){
     mostrarCarrito();
     
 }
-//Se crean los productos
-let acumulador = ``;
-productos.forEach((producto)=>{
-    acumulador += `<div class="card">
-    <img class="card-img-top img-fluid" src="${producto.imagen}" alt="${producto.nombre}">
-    <div class="card-body">
-      <h5 class="card-title">${producto.nombre}</h5>
-      <p class="card-text">$${producto.precio}</p>
-    </div>
+function generarCards(){
+    //Se crean los productos
+    let acumulador = ``;
+    productos.forEach((producto)=>{
+        acumulador += `<div class="col mb-5" id="${producto.nombre}">
+        <div class="card">
+        <img class="card-img-top img-fluid" src="${producto.imagen}" alt="${producto.nombre}">
+        <div class="card-body">
+        <h5 class="card-title">${producto.nombre}</h5>
+        <p class="card-text">$${producto.precio}</p>
+        </div>
     
-    <div class="card-body text-center">
-      <button class="btn" onclick="agregarCarrito('${producto.nombre}')">Agregar al carrito</button>
+        <div class="card-body text-center">
+        <button class="btn" onclick="agregarCarrito('${producto.nombre}')">Agregar al carrito</button>
+        </div>
     </div>
-</div>`
-})
-document.getElementById("listaProductos").innerHTML = acumulador;
+    </div>`
+    })
+    document.getElementById("listaProductos").innerHTML = acumulador;
+}
+
 //Se encarga de mostrar el carrito en la web
 function mostrarCarrito(){
     if(carrito.cantidad === 0){
@@ -133,13 +147,60 @@ function mostrarCarrito(){
     </td>
     
   </tr>`
-        
+   
     document.getElementById("carrito").innerHTML = tabla;
     
     });
-    
+    document.getElementById("carrito").innerHTML+=`<tr>
+    <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#modalcontenedor">
+  Confirmar compra
+</button>`;
+    generarModal();
+}
+function generarModal() {
+    let resumen = ``;
+    let total = 0;
+    carrito.productos.forEach(producto => {
+        resumen += `<tr>
+        <td>${producto.nombre}</td>
+        <td>${producto.cantidad}</td>
+        <td>${producto.precio * producto.cantidad}</td>
+    </tr>`
+        total += producto.cantidad * producto.precio;
+    });
+    document.getElementById("modalcontenedor").innerHTML = `
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="ModalLabel">Resumen de su compra</h5>
+          <button type="button" class="btn" data-bs-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+        <table class="table table-hover">
+            <tr>
+                <th>Nombre </th>
+                <th>Cantidad</th>
+                <th>Precio </th>
+            </tr>
+            ${resumen}
+            <tr>
+                <td></td>
+                <td></td>
+                <td>Total: ${total} </td>
+            </tr>
+        </table>    
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+          <button type="button" class="btn btn-primary" data-bs-dismiss="modal"">Confirmar compra</button>
+        </div>
+      </div>
+    </div>`;
 }
 function quitarCarrito(producto){
     carrito.eliminarProducto(producto);
     mostrarCarrito();
 }
+generarCards();
